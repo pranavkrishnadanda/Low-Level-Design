f66a8c13803e6aa1a5c92a750c94011a8ff07da2
Pranav Krishna Danda <pranavkrishna317@gmail.com>
2026-03-29 16:54:14 +0530

    Added New URLs

--- app/main.py
+++ app/main.py
@@ -5,3 +5,20 @@ app = FastAPI()
 def read_root():
     return {"Hello": "World"}
 
+@app.post('/AddTodos')
+def ImplmentTodos():
+    # Logic for Implmenting Todos
+    
+    return {"status": 200,"success" : True}
+
+@app.get('GetTodos')
+def GetTodos():
+    # Logic for Getting Todos
+    
+    return {"status": 200,"success" : True}
+    
+@app.post('/UpdateTodos')
+def UpdateTodos():
+    #Logic for Updating Todos
+    
+    return {"status": 200,"success" : True}
